db1bebcc6a06fa796970b11280ea3e359e08c2d2
Julian <Julian_Ullrich@live.de>
2025-12-03 13:39:55 +0100

    day2part1

--- day2/Day2.go
+++ day2/Day2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,10 +14,28 @@ func main() {
 	}
 
 	text := string(file)
-	lines := strings.Split(text, "\n")
+	lines := strings.Split(text, ",")
+	wrongIDSum := 0
 
 	for _, line := range lines {
-		fmt.Println(line)
+		values := strings.Split(line, "-")
+		start, _ := strconv.Atoi(values[0])
+		end, _ := strconv.Atoi(values[1])
+		for i := start; i <= end; i++ {
+			numString := strconv.Itoa(i)
+			numStringLen := len(numString)
+			if numStringLen%2 != 0 {
+				continue
+			}
+			if numString[0:numStringLen/2] == numString[numStringLen/2:] {
+				wrongIDSum += i
+
+			}
+
+		}
+
 	}
+
+	fmt.Println(wrongIDSum)
 
 }
